7b0a382ba87ced0701a3e5bf39ace32d3b4722b3
Hilson <hilson.alex234@gmail.com>
2021-05-24 22:26:48 -0300

    modify jar folder

--- ipsis_literis.bat
+++ ipsis_literis.bat
@@ -1,4 +1,6 @@
-javacc src\main\java\scanner\compiler\IpsisLiteris.jj
+call javacc src\main\java\scanner\compiler\IpsisLiteris.jj
+echo.
 echo "Compilador da linguagem 2021.1 (Ipsis Literis) gerado"
 echo "Pronto para compilar"
+echo.
 pause
