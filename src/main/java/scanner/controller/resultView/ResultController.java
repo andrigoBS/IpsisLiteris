dd34cf1f2f7c0cb09aded65cd88d42a99d6d9ac7
andrigoBS <andrigo2000@gmail.com>
2021-06-22 00:32:11 -0300

    Console javafx (no understand special characters)

--- src/main/java/scanner/controller/resultView/ResultController.java
+++ src/main/java/scanner/controller/resultView/ResultController.java
@@ -1,24 +1,73 @@
 package scanner.controller.resultView;
 
+import javafx.fxml.Initializable;
+import javafx.scene.input.KeyCode;
+import org.fxmisc.richtext.CodeArea;
 import scanner.controller.AbstractController;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 
-public class ResultController extends AbstractController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ResultController extends AbstractController implements Initializable {
     @FXML
-    private TextArea textView;
+    private CodeArea codeArea;
 
     @FXML
     private Label rowColView;
 
+    private int inputPosition;
+
+    private String backupText;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        codeArea.setEditable(false);
+        codeArea.caretPositionProperty().addListener(((observableValue, oldValue, newValue) -> {
+            if(newValue < inputPosition){
+                codeArea.displaceCaret(codeArea.getText().length());
+            }
+        }));
+        codeArea.setOnKeyPressed(keyEvent -> {
+            if(codeArea.isEditable()){
+                if(keyEvent.getCode().equals(KeyCode.BACK_SPACE)) {
+                    if (codeArea.getCaretPosition() >= inputPosition) {
+                        backupText = backupText.substring(0, codeArea.getText().length());
+                    }
+                }else if(keyEvent.getCode().equals(KeyCode.ENTER)){
+                    codeArea.setEditable(false);
+                    backupText += keyEvent.getText();
+                }else if(keyEvent.getCode().isLetterKey() || keyEvent.getCode().isDigitKey() || keyEvent.getCode().isWhitespaceKey()){
+                    if (codeArea.getCaretPosition() >= inputPosition) {
+                        backupText += keyEvent.getText();
+                    }
+                }
+            }
+        });
+        codeArea.setOnKeyReleased(keyEvent -> {
+            if (codeArea.isEditable()) {
+                codeArea.replaceText(backupText);
+            }
+        });
+//        setText("console\nlogsom\n"); //teste console
+//        enableInput(); //teste console
+    }
+
+    public void enableInput(){
+        backupText = codeArea.getText();
+        inputPosition = backupText.length();
+        codeArea.setEditable(true);
+        codeArea.displaceCaret(inputPosition);
+    }
+
     public void setText(String text){
         clear();
-        textView.setText(text);
+        codeArea.replaceText(text);
     }
 
     public void clear(){
-        textView.clear();
+        codeArea.clear();
     }
 
     public void setRowCol(int row, int col){
